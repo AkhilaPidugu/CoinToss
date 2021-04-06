1c7804b225dbe08d8e40375e1cb89c1241d186e8
unknown <akhilapidugu99@gmail.com>
2021-04-06 08:17:06 +0530

    Implemented the probability equal check of head and tail of a coin

--- src/main/java/Coin.java
+++ src/main/java/Coin.java
@@ -1,2 +1,18 @@
 public class Coin {
+
+    private  double headProbability;
+    private double tailProbability;
+    public Coin(double headProbability, double tailProbability) {
+        this.headProbability = headProbability;
+        this.tailProbability = tailProbability;
+    }
+
+    public boolean probabilityIsEqual() {
+        if(headProbability == tailProbability) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
--- src/test/java/CoinTest.java
+++ src/test/java/CoinTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CoinTest {
+    @Test
+    public void testIfHeadProbabilityOfACoinIsEqualToTailProbability() {
+        boolean actualValue;
+        double headProbability = 0.5;
+        double tailProbability = 0.5;
+        Coin coin= new Coin(headProbability,tailProbability);
+
+        actualValue = coin.probabilityIsEqual();
+
+        assertTrue(actualValue);
+    }
+
 }
