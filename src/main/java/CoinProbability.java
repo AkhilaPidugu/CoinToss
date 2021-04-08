4e2a17cfdde12473e4f4de678e828a8694c8efe7
unknown <akhilapidugu99@gmail.com>
2021-04-08 16:52:19 +0530

    Implemented probobality of tossing two coins simultaneously with both sides as head

--- src/main/java/CoinProbability.java
+++ src/main/java/CoinProbability.java
@@ -1,12 +1,16 @@
-import java.util.Objects;
-
 public class CoinProbability {
 
-    private  double headProbability;
+    private double side;
+    private double headProbability;
     private double tailProbability;
+
     public CoinProbability(double headProbability, double tailProbability) {
         this.headProbability = headProbability;
         this.tailProbability = tailProbability;
+    }
+
+    public CoinProbability(double side) {
+        this.side = side;
     }
 
     @Override
@@ -15,4 +19,7 @@ public class CoinProbability {
         return Double.compare(coinProbability.headProbability, this.tailProbability) == 0;
     }
 
+    public double probabilityOfTwoEventsThatOccurTogether( CoinProbability coinProbability) {
+        return this.side*coinProbability.side;
+    }
 }
--- src/test/java/CoinProbabilityTest.java
+++ src/test/java/CoinProbabilityTest.java
@@ -3,13 +3,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CoinProbabilityTest {
+    double headProbability = 0.5;
+    double tailProbability = 0.5;
     @Test
     public void testIfHeadProbabilityOfACoinIsEqualToTailProbability() {
-        double headProbability = 0.5;
-        double tailProbability = 0.5;
         CoinProbability coinProbability = new CoinProbability(headProbability,tailProbability);
 
         assertEquals(coinProbability, coinProbability);
     }
+
+    @Test
+    void testIfProbabilityOfTossingTwoCoinsSimultaneouslyWithBothTheSidesAsHeadIs0Point25() {
+        double expectedValue = 0.25;
+        CoinProbability coin1headProbability = new CoinProbability(headProbability);
+        CoinProbability coin2headProbability = new CoinProbability(headProbability);
+
+        double actualValue = coin2headProbability.probabilityOfTwoEventsThatOccurTogether(coin1headProbability);
+
+        assertEquals(expectedValue,actualValue);
+    }
+
 
 }
